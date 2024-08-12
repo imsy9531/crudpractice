4ba14c4d97474bda9ca803b7b296fc3cb7b30636
seongyoonlim <seongyoonhere@gmail.com>
2024-08-12 19:21:37 +0900

    init

---
+++ src/main/java/crudpractice/crudpractice/CrudpracticeApplication.java
@@ -0,0 +1,13 @@
+package crudpractice.crudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpracticeApplication.class, args);
+	}
+
+}
